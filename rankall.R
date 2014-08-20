24ea84c1db03761892fa56e51260d55809db9358
gkumar10 <gaurav.kumar@me.com>
2014-08-19 17:14:35 -0700

    ..

---
+++ rankall.R
@@ -0,0 +1,71 @@
+rankall <- function(outcome, num = "best") {
+  ## Read outcome data
+  #initialize
+  r <- NULL
+  c <- NULL
+  rf1 <- NULL
+  rf2 <- NULL 
+  n <- NULL #rank
+  readfile <- NULL
+  rfile <- NULL
+  f <- FALSE # flag for condition where num > nrow
+  u <- NULL #unique states
+  hos <- NULL
+  st <- NULL
+  
+  
+  ## Read outcome data
+  readfile <- read.csv("~/Coursera/HospitalQuality/outcome-of-care-measures.csv", colClasses="character", header=TRUE)
+  rfile <- readfile[,c(2,7,11,17,23)] #read 5 columns relevant to this function
+  u <- unique(rfile[,2]) #returns vector with 54 states
+  
+  ## Check that state and outcome are valid
+  if (outcome == "heart attack") {
+    c <- 3 #column #3 in rfile
+  } else {
+    if (outcome == "heart failure") {
+      c <- 4
+    } else {
+      if (outcome == "pneumonia") {
+        c <- 5
+      } else {
+        geterrmessage()
+        stop("invalid outcome")
+      }
+    }
+  }
+    
+  for (i in 1:length(u)) {
+    
+    r <- NULL
+    f <- FALSE
+    
+    r <- grep(u[i], rfile[,2], ignore.case=TRUE)
+    
+    if (is.character(num) & (as.character(num) == "best")) {
+      n <- 1
+    } else {
+      if (is.character(num) & (as.character(num) == "worst")) {
+        n <- length(r) - length(grep("not available", rfile[r,c], ignore.case=TRUE))
+      } else {
+        if (is.numeric(num) & (as.numeric(num) > length(r))) {
+          f <- TRUE
+        } else {
+          n <- as.numeric(num)
+        }
+      }
+    }
+      
+    if (f) {
+      #print(<NA>)
+      hos[i] <- "<NA>"
+      st[i] <- rf[n,2]
+    } else {
+      rf1 <- rfile[r,]
+      rf2 <- suppressWarnings(na.omit(rf1[order(as.numeric(rf1[,c]),rf1[,1], na.last=TRUE),]))
+      hos[i] <- as.character(rf2[n,1])
+      st[i] <- as.character(rf2[n,2])
+    }
+  }
+  data.frame(hos, st, stringsAsFactors = FALSE)
+}
